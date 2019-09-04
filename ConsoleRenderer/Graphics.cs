5c416d330716e31c00ca86919ec10ead98bfda59
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-09-04 15:09:15 +0200

    wip

---
+++ ConsoleRenderer/Graphics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRenderer
+{
+
+    class Graphics
+    {
+        static void PolygonFill(ref char[,] screen, List<Vector2> points)
+        {
+            int nodes, pixelX, pixelY, i, j, swap;
+            int[] nodeX = new int[Constants.MAX_POLY_CORNERS];
+
+            for (pixelY=0; pixelY<Console.WindowHeight; pixelY++)
+            {
+
+            }
+        }
+    }
+}
